f7347eb3e712bc78959f61bd74895e227c73597a
silvaluam <152218519+silvaluam@users.noreply.github.com>
2025-08-19 14:42:28 -0300

    19.08

---
+++ main.js
@@ -0,0 +1,42 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho(){
+    if(tamanhoSenha > 1){
+        tamanhoSenha = tamanhoSenha - 1
+    }
+    numeroSenha.textContent=tamanhoSenha;
+    geraSenha();
+}
+
+function aumentaTamanho(){
+    if(tamanhoSenha < 20){
+        tamanhoSenha = tamanhoSenha + 1
+    }
+    numeroSenha.textContent=tamanhoSenha;
+    geraSenha();
+}
+
+const campoSenha = document.querySelector( '#campo-senha')
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!@#%+=¨';
+geraSenha();
+
+function geraSenha() {
+    let senha = '';
+    for (let i=0; i<tamanhoSenha; i++){
+        let numero aleatorio = Math.random()*letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha = letrasMaiusculas[numeroAleatorio]
+    }
+campoSenha.value = senha;
+
+}
