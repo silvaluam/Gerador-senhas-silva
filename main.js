546fba6e80edc97534ccae6c5f49895ef0581902
silvaluam <152218519+silvaluam@users.noreply.github.com>
2025-08-26 14:06:24 -0300

    26.08

--- main.js
+++ main.js
@@ -31,6 +31,23 @@ const simbolos = '!@#%+=¨';
 geraSenha();
 
 function geraSenha() {
+let alfbeto = '';
+if (checkbox[0].checked){
+    alfabeto = alfabeto +letrasMaiusculas;
+}
+
+if (checkbox[1].checked){
+    alfabeto = alfabeto +letrasMinusculas;
+}
+
+if (checkbox[2].checked){
+    alfabeto = alfabeto + numeros;
+}
+
+if (checkbox[3].checked){
+    alfabeto = alfabeto +simbolos;
+}
+
     let senha = '';
     for (let i = 0; i < tamanhoSenha; i++) {
         let numeroAleatorio = Math.random() * letrasMaiusculas.length;
