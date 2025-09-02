4afc3462469724e49a762a48c208b4b3b4be3983
silvaluam <152218519+silvaluam@users.noreply.github.com>
2025-09-02 13:44:53 -0300

    02.09

--- main.js
+++ main.js
@@ -64,3 +64,13 @@ senha = senha + alfabeto[numeroAleatorio];
 campoSenha.value = senha;
 classificaSenha();
 }
+function classificaSenha() {
+forcaSenha.classList.remove('fraca', 'media', 'forte');
+if (tamanhoSenha > 11) {
+forcaSenha.classList.add('forte');
+} else if (tamanhoSenha > 5 && tamanhoSenha < 12) {
+forcaSenha.classList.add('media');
+} else if (tamanhoSenha <= 5) {
+forcaSenha.classList.add('fraca');
+}
+}
